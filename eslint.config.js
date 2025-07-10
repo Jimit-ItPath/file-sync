d10b7c5a3e6c926da4481c7ceb8766b8a5afefd4
jimit shah <jimits@itpathsolutions.com>
2025-07-10 14:42:28 +0530

    feat: first commit done

---
+++ eslint.config.js
@@ -0,0 +1,31 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import { globalIgnores } from 'eslint/config';
+import prettier from 'eslint-config-prettier';
+import mantineConfig from 'eslint-config-mantine';
+
+export default tseslint.config([
+  globalIgnores(['dist']),
+  {
+    files: ['**/*.{ts,tsx}'],
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.recommended,
+      reactHooks.configs['recommended-latest'],
+      reactRefresh.configs.vite,
+      prettier, // Disables ESLint rules that conflict with Prettier
+      mantineConfig,
+    ],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    rules: {
+      '@typescript-eslint/no-explicit-any': 0,
+      'no-console': 0,
+    },
+  },
+]);
